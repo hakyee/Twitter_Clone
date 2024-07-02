a3fddab6de8f93d1fe0e0028d7466ffe355dadc5
hakyee <wkbkmj1@naver.com>
2024-07-02 19:21:38 +0900

    Setup

---
+++ src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRKxq_ku45zwFElMbdHTddFfvUtWbYf_U",
+  authDomain: "twitter-clone-97a0b.firebaseapp.com",
+  projectId: "twitter-clone-97a0b",
+  storageBucket: "twitter-clone-97a0b.appspot.com",
+  messagingSenderId: "79193524121",
+  appId: "1:79193524121:web:3a89b60472d2a37d7ab508",
+};
+
+const app = initializeApp(firebaseConfig);
