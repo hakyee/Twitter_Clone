e7b24b4f837d3cdba64cf732543cf6072a9d880a
hakyee <wkbkmj1@naver.com>
2024-07-22 19:29:41 +0900

    Username Edit && Deploy

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -44,8 +44,67 @@ const AvatarInput = styled.input`
   display: none;
 `;
 
+const UserName = styled.div`
+  display: flex;
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
 const Name = styled.span`
   font-size: 22px;
+`;
+
+const EditName = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NameInput = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 60%;
+  height: 80%;
+  font-size: 16px;
+  margin-right: 10px;
+`;
+
+const CancelBtn = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  height: 80%;
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
+const EditBtn = styled.button`
+  background-color: #1d9cf0ea;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  height: 80%;
+  cursor: pointer;
+`;
+
+const Edit = styled.span`
+  display: flex;
+  align-items: center;
+  position: relative;
+  left: 5px;
+  cursor: pointer;
 `;
 
 const Tweets = styled.div`
@@ -58,8 +117,10 @@ const Tweets = styled.div`
 
 export default function Profile() {
   const user = auth.currentUser;
+  const [name, setName] = useState(user?.displayName ?? "Anonymous");
   const [avatar, setAvatar] = useState(user?.photoURL);
   const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [editing, setEditing] = useState(false);
   const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (!user) return;
@@ -97,7 +158,22 @@ export default function Profile() {
   };
   useEffect(() => {
     fetchTweets();
-  });
+  }, []);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onEditName = () => {
+    setEditing(true);
+  };
+  const onCancel = () => {
+    setEditing(false);
+  };
+  const onEdit = async () => {
+    if (user && name) {
+      await updateProfile(user, { displayName: name });
+      setEditing(false);
+    }
+  };
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
@@ -123,7 +199,31 @@ export default function Profile() {
         type="file"
         accept="image/*"
       />
-      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      {editing ? (
+        <EditName>
+          <NameInput
+            onChange={onChange}
+            placeholder={user?.displayName ?? "Anonymous"}
+            value={name}
+          />
+          <CancelBtn onClick={onCancel}>Cancel</CancelBtn>
+          <EditBtn onClick={onEdit}>Edit</EditBtn>
+        </EditName>
+      ) : (
+        <UserName>
+          <Name>{user?.displayName ?? "Anonymous"}</Name>
+          <Edit onClick={onEditName}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+              <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+            </svg>
+          </Edit>
+        </UserName>
+      )}
       <Tweets>
         {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
